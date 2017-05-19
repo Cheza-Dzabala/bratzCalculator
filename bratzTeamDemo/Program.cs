37976ece20b1b4a682db35c5ca5a42435bb0a629
Cheza <dzabalamacheza@gmail.com>
2017-05-19 23:41:12 +0200

    Initial Commit

---
+++ bratzTeamDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bratzTeamDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Matrix");
+
+            string value = Console.ReadLine();
+
+            Console.WriteLine("You typed " + value);
+
+            Console.ReadLine();
+        }
+    }
+}
