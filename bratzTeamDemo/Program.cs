77447155f94f0d6c4e7c445345df7eb190c744f0
liwu <jeremiahchienda@gmail.com>
2017-05-20 03:28:10 +0200

    whatever

--- bratzTeamDemo/Program.cs
+++ bratzTeamDemo/Program.cs
@@ -10,11 +10,7 @@ namespace bratzTeamDemo
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Welcome To The Matrixie");
-=======
             Console.WriteLine("Welcome To The Matrix Calculator");
->>>>>>> 9f40d2e... Just playing about
 
             string value = Console.ReadLine();
 
