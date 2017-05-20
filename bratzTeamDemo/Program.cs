2a1cd981d09d241401a2ba205528130aa2e04c0b
liwu <jeremiahchienda@gmail.com>
2017-05-20 03:39:10 +0200

    Just playing about
    Conflicts:
    	bratzTeamDemo/Program.cs
    
    Conflicts:
    	bratzTeamDemo/Program.cs
    
    Conflicts:
    	bratzTeamDemo/Program.cs

--- bratzTeamDemo/Program.cs
+++ bratzTeamDemo/Program.cs
@@ -10,8 +10,20 @@ namespace bratzTeamDemo
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
 
             Console.WriteLine("Welcome To The Matrix Calculator");
+=======
+<<<<<<< HEAD
+            Console.WriteLine("Welcome To The Matrix");
+=======
+<<<<<<< HEAD
+            Console.WriteLine("Welcome To The Matrixie");
+=======
+            Console.WriteLine("Welcome To The Matrix Calculator");
+>>>>>>> 9f40d2e... Just playing about
+>>>>>>> b986106... Just playing about
+>>>>>>> 23e1323... Just playing about
 
             string value = Console.ReadLine();
 
