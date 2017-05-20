b986106511b10e57ff09c6064f2a45946ae32ff3
liwu <jeremiahchienda@gmail.com>
2017-05-20 03:27:25 +0200

    Just playing about
    Conflicts:
    	bratzTeamDemo/Program.cs

--- bratzTeamDemo/Program.cs
+++ bratzTeamDemo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,11 @@ namespace bratzTeamDemo
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Welcome To The Matrixie");
+=======
+            Console.WriteLine("Welcome To The Matrix Calculator");
+>>>>>>> 9f40d2e... Just playing about
 
             string value = Console.ReadLine();
 
