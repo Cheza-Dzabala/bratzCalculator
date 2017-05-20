c366f667475cdf32e6fe58ef1c83f1ba8780fa7a
Cheza <dzabalamacheza@gmail.com>
2017-05-20 04:05:11 +0200

    Class Scaffolding

--- bratzTeamDemo/Program.cs
+++ bratzTeamDemo/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,36 @@ namespace bratzTeamDemo
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("This is the freshest!");
+            Console.WriteLine("Please input first digit.. ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input second digit.. ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select Method: ");
+            Console.WriteLine("1. Subtract");
+            Console.WriteLine("2. Add");
+            Console.WriteLine("3. Divide");
+            Console.WriteLine("4. Multiply");
+
+
+            int selection = int.Parse(Console.ReadLine());
+
+            switch(selection)
+            {
+                case 1:
+                    Console.WriteLine("Subtraction");
+                    break;
+                case 2:
+                    Console.WriteLine("Addition");
+                    break;
+                case 3:
+                    Console.WriteLine("Division");
+                    break;
+                case 4:
+                    Console.WriteLine("Multiplication");
+                    break;
+            }
         }
     }
 }
---
+++ bratzTeamDemo/calculatorClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bratzTeamDemo
+{
+    class calculatorClass
+    {
+        private int numOne;
+        private int numTwo;
+
+        public  int subtraction(int num1, int num2)
+        {
+            numOne = num1;
+            numTwo = num2;
+
+            return numOne - numTwo;
+        }
+    }
+}
