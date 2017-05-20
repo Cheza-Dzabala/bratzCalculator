696703076f5edfbeaf50b09eaf3b79b8a5873fe6
liwu <jeremiahchienda@gmail.com>
2017-05-20 03:40:25 +0200

    starting afresh

--- bratzTeamDemo/Program.cs
+++ bratzTeamDemo/Program.cs
@@ -10,35 +10,7 @@ namespace bratzTeamDemo
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-            Console.WriteLine("Welcome To The Matrix Calculator");
-=======
-=======
->>>>>>> 9604e23... whatever
-<<<<<<< HEAD
-            Console.WriteLine("Welcome To The Matrix");
-=======
-<<<<<<< HEAD
-            Console.WriteLine("Welcome To The Matrixie");
-=======
-            Console.WriteLine("Welcome To The Matrix Calculator");
->>>>>>> 9f40d2e... Just playing about
->>>>>>> b986106... Just playing about
-<<<<<<< HEAD
->>>>>>> 23e1323... Just playing about
-=======
-=======
-            Console.WriteLine("Welcome To The Matrix Calculator");
->>>>>>> 7744715... whatever
->>>>>>> 9604e23... whatever
-
-            string value = Console.ReadLine();
-
-            Console.WriteLine("You typed This " + value);
-
-            Console.ReadLine();
+			Console.WriteLine("This is the freshest");
         }
     }
 }
